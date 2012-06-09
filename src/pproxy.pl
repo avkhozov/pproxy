90f99260fd91a3d7a8fac1839b4e2bab7a0e7902
Andrey Khozov <avkhozov@gmail.com>
2012-06-09 19:07:59 +0600

    Write streams to pcap file.

--- src/pproxy.pl
+++ src/pproxy.pl
@@ -6,52 +6,69 @@ use Mojo::Log;
 use Net::PcapWriter;
 
 my $conf = do 'pproxy.conf';
-my $log = Mojo::Log->new;
+my $log = Mojo::Log->new(path => $conf->{log});
+my $pcap_file = $conf->{pcap} // 'data.pcap';
 
 my $lister = $conf->{listen};
 my $proxy = $conf->{proxy};
 
 my $connections = {};
 
+open my $fh, '>', $pcap_file or die "Error while write $pcap_file: $!";
+my $old = select $fh; $|=1; select $old;
+my $pcap_writer = Net::PcapWriter->new($fh);
+
 for my $port (keys %$proxy) {
     $log->info("Start listen on $port");
+    my $remote_addr = $proxy->{$port}->{address};
+    my $remote_port = $proxy->{$port}->{port};
     my $server = Mojo::IOLoop->server({port => $port} => sub {
         my ($loop, $stream, $id) = @_;
         $stream->on(close => sub {
             $log->debug("Closed $id");
-            if (my $orign = $connections->{$id}) {
+            $connections->{$id}->{pcap_connection}->shutdown(0);
+            undef $connections->{$id}->{pcap_connection};
+            if (my $orign = $connections->{$id}->{orign_id}) {
                 if (my $orign_stream = Mojo::IOLoop->stream($orign)) {
-                    return $orign_stream->close;
+                    $orign_stream->close;
                 }
             }
+            delete $connections->{$id};
         });
         $stream->on(error => sub {
             my ($stream, $error) = @_;
-            if (my $orign = $connections->{$id}) {
+            $log->debug("Error in $id: $error");
+            if (my $orign = $connections->{$id}->{orign_id}) {
                 if (my $orign_stream = Mojo::IOLoop->stream($orign)) {
-                    return $orign_stream->close;
+                    $orign_stream->close;
                 }
             }
         });
         $stream->on(read => sub {
             my ($stream, $chunk) = @_;
             $log->debug("Read data on $id: $chunk");
-            if (my $orign = $connections->{$id}) {
+            $connections->{$id}->{pcap_connection}->write(0, $chunk);
+            $connections->{$id}->{pcap_connection}->ack(1);
+            # Check for existsting connection
+            if (my $orign = $connections->{$id}->{orign_id}) {
                 if (my $orign_stream = Mojo::IOLoop->stream($orign)) {
                     return $orign_stream->write($chunk);
                 }
                 return;
             }
-            my $remote_addr = $proxy->{$port}->{address};
-            my $remote_port = $proxy->{$port}->{port};
+            # Open new connection
             $log->debug("Connecting to $remote_addr:$remote_port");
             my $orign = Mojo::IOLoop->client({address => $remote_addr,
                                                 port => $remote_port} => sub {
                 my ($loop, $err, $stream) = @_;
+                # Inactivity timeout for stream
+                $stream->timeout(5 * 60);
                 $stream->on(read => sub {
                     my ($stream, $chunk) = @_;
                     $log->debug("Read data from orign $id: $chunk");
                     Mojo::IOLoop->stream($id)->write($chunk);
+                    $connections->{$id}->{pcap_connection}->write(1, $chunk);
+                    $connections->{$id}->{pcap_connection}->ack(0);
                 });
                 $stream->on(timeout => sub {
                     $log->debug("timeout: orign $id");
@@ -69,9 +86,11 @@ for my $port (keys %$proxy) {
                 $log->debug("Connected to $remote_addr:$remote_port");
                 $stream->write($chunk);
             });
-            $connections->{$id} = $orign;
+            $connections->{$id}->{orign_id} = $orign;
         });
         $log->debug("Starting stream: $id");
+        my $conn = $pcap_writer->tcp_conn($stream->handle->peerhost, $stream->handle->peerport, $remote_addr, $remote_port);
+        $connections->{$id}->{pcap_connection} = $conn;
     });
 }
 
