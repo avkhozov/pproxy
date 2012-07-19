eb47b57e64aa79d8807a624fb657ac91b68a07b2
Andrey Khozov <avkhozov@gmail.com>
2012-07-19 23:31:01 +0600

    Remove unnecessary PcapWriter.

--- src/pproxy.pl
+++ src/pproxy.pl
@@ -3,27 +3,13 @@
 use strict;
 use Mojo::IOLoop;
 use Mojo::Log;
-use Net::PcapWriter;
 use PProxy::IDS;
 
 my $conf = do 'pproxy.conf' or die 'Invalid configuration file';
-my $log = Mojo::Log->new(path => $conf->{log}, level => 'info');
-my $dump_pcap = $conf->{dump_pcap} // 0;
-my $pcap_file = $conf->{pcap} // 'data.pcap';
+my $log = Mojo::Log->new(path => $conf->{log}, level => 'debug');
 my $rules_dir = $conf->{rules} // 'rules';
-
-my $lister = $conf->{listen};
 my $proxy = $conf->{proxy};
-
 my $connections = {};
-
-my $pcap_writer;
-if ($dump_pcap)
-{
-    open my $fh, '>', $pcap_file or die "Error on write to $pcap_file: $!";
-    my $old = select $fh; $|=1; select $old;
-    $pcap_writer = Net::PcapWriter->new($fh);
-}
 
 my $ids = PProxy::IDS->new($rules_dir);
 
@@ -35,8 +21,6 @@ for my $port (keys %$proxy) {
         my ($loop, $stream, $id) = @_;
         $stream->on(close => sub {
             $log->debug("Closed $id");
-            $connections->{$id}->{pcap_connection}->shutdown(0) if $dump_pcap;
-            undef $connections->{$id}->{pcap_connection} if $dump_pcap;
             if (my $orign = $connections->{$id}->{orign_id}) {
                 if (my $orign_stream = Mojo::IOLoop->stream($orign)) {
                     $orign_stream->close;
@@ -58,8 +42,6 @@ for my $port (keys %$proxy) {
             $log->debug("Read data on $id: $chunk");
             my $ids_action = $ids->process_chunk($chunk);
             $log->debug("Action for this chunk is $ids_action");
-            $connections->{$id}->{pcap_connection}->write(0, $chunk) if $dump_pcap;
-            $connections->{$id}->{pcap_connection}->ack(1) if $dump_pcap;
             return if $ids_action eq 'drop';
             # Check for existsting connection
             if (my $orign = $connections->{$id}->{orign_id}) {
@@ -85,8 +67,6 @@ for my $port (keys %$proxy) {
                     $log->debug("Read data from orign $id: $chunk");
                     my $ids_action = $ids->process_chunk($chunk);
                     $log->debug("Action for this chunk is $ids_action");
-                    $connections->{$id}->{pcap_connection}->write(1, $chunk) if $dump_pcap;
-                    $connections->{$id}->{pcap_connection}->ack(0) if $dump_pcap;
                     return if $ids_action eq 'drop';
                     Mojo::IOLoop->stream($id)->write($chunk);
                 });
@@ -109,11 +89,6 @@ for my $port (keys %$proxy) {
             $connections->{$id}->{orign_id} = $orign;
         });
         $log->debug("Starting stream: $id");
-        if ($dump_pcap)
-        {
-            my $conn = $pcap_writer->tcp_conn($stream->handle->peerhost, $stream->handle->peerport, $remote_addr, $remote_port);
-            $connections->{$id}->{pcap_connection} = $conn;
-        }
     });
 }
 
@@ -124,5 +99,4 @@ sub reload_rules
 }
 
 Mojo::IOLoop->recurring(5 => \&reload_rules);
-
 Mojo::IOLoop->start;
